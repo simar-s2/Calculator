0cab1058f20f490d8a324afe8a9d4f3b31a5e3f0
simar-s2 <simarjot2468@gmail.com>
2023-12-04 00:11:32 +0300

    Add delete button functionality

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 // selecting DOM elements
 const numberbtns = document.querySelectorAll('.number');
 const operationbtns = document.querySelectorAll('.operation')
-const clearbtn = document.getElementById('clearbtn');
-const deletebtn = document.getElementById('deletebtn');
+const clearbtn = document.getElementById('clear');
+const deletebtn = document.getElementById('delete');
 const screen = document.querySelector('.screen');
 
 numberbtns.forEach((btn) =>
     btn.addEventListener('click', () => appendNumber(btn.textContent))
 )
 
+deletebtn.addEventListener('click', removeNumber);
+
 function appendNumber(number) {
     screen.textContent += number;
+}
+
+function removeNumber() {
+    screen.textContent = screen.textContent.toString().slice(0, -1)
 }
