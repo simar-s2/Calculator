4a666c2a1ee38d5340413d881eb19f7855e4f3cb
simar-s2 <simarjot2468@gmail.com>
2023-12-05 00:11:49 +0300

    Make basics work

--- app.js
+++ app.js
@@ -7,14 +7,15 @@ const screen = document.getElementById('screen');
 const equalsbtn = document.getElementById('equals');
 const pointbtn = document.getElementById('point');
 
-let num1 = ''
-let num2 = ''
+let num1 = '';
+let num2 = '';
+let operand = '';
 
 // Event Listeners
-equalsbtn.addEventListener('click', () => evaluate(num1, num2));
+equalsbtn.addEventListener('click', () => evaluate(operand, num1, num2));
 deletebtn.addEventListener('click', removeNumber);
 clearbtn.addEventListener('click', clearScreen);
-pointbtn.addEventListener('click', addPoint);
+// pointbtn.addEventListener('click', addPoint);
 
 numberbtns.forEach((btn) =>
     btn.addEventListener('click', () => appendNumber(btn.textContent))
@@ -37,18 +38,50 @@ function removeNumber() {
 // Clears Screen
 function clearScreen () {
     screen.textContent = '';
+    num1 = '';
+    num2 = '';
+    operand = '';
 }
 
 // Selects operation to perform
 function addOperation(operation) {
-    screen.textContent += operation;
-}
-
-function addPoint() {
-    if(screen.textContent.includes('.')){
+    if(operand !== ''){
         return;
     }
     else {
-        screen.textContent += '.';
+        num1 = screen.textContent;
+        screen.textContent += operation;
+        operand = operation;
+        screen.textContent = '';
     }
 }
+
+function evaluate(operand, num1, num2) {
+    num2 += screen.textContent;
+    switch (operand) {
+        case '+':
+            screen.textContent = num1 + num2;
+            break;
+        case '−':
+            screen.textContent = num1 - num2;
+            break;
+        case '×':
+            screen.textContent = num1 * num2;
+            break;
+        case '÷':
+            screen.textContent = num1 / num2;
+        default:
+            break;
+    }
+}
+
+// Adds a decimal point
+// If there is already one point the user cannot add one more
+// function addPoint() {
+//     if(screen.textContent.includes('.')){
+//         return;
+//     }
+//     else {
+//         screen.textContent += '.';
+//     }
+// }
