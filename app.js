a96f386eb88f992ee6b51456a34a179c1d851b27
simar-s2 <simarjot2468@gmail.com>
2023-12-04 20:39:28 +0300

    Added point button functionality

--- app.js
+++ app.js
@@ -3,23 +3,52 @@ const numberbtns = document.querySelectorAll('.number');
 const operationbtns = document.querySelectorAll('.operation')
 const clearbtn = document.getElementById('clear');
 const deletebtn = document.getElementById('delete');
-const screen = document.querySelector('.screen');
+const screen = document.getElementById('screen');
+const equalsbtn = document.getElementById('equals');
+const pointbtn = document.getElementById('point');
+
+let num1 = ''
+let num2 = ''
+
+// Event Listeners
+equalsbtn.addEventListener('click', () => evaluate(num1, num2));
+deletebtn.addEventListener('click', removeNumber);
+clearbtn.addEventListener('click', clearScreen);
+pointbtn.addEventListener('click', addPoint);
 
 numberbtns.forEach((btn) =>
     btn.addEventListener('click', () => appendNumber(btn.textContent))
 )
 
-deletebtn.addEventListener('click', removeNumber);
-clearbtn.addEventListener('click', clearScreen);
+operationbtns.forEach((btn) => 
+    btn.addEventListener('click', () => addOperation(btn.textContent))
+)
 
+// Adds number to the screen
 function appendNumber(number) {
     screen.textContent += number;
 }
 
+// Removes a number from the screen
 function removeNumber() {
     screen.textContent = screen.textContent.toString().slice(0, -1);
 }
 
+// Clears Screen
 function clearScreen () {
     screen.textContent = '';
+}
+
+// Selects operation to perform
+function addOperation(operation) {
+    screen.textContent += operation;
+}
+
+function addPoint() {
+    if(screen.textContent.includes('.')){
+        return;
+    }
+    else {
+        screen.textContent += '.';
+    }
 }
